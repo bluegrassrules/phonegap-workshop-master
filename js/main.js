1dc47d42effe9a700a3628d2bc581a6475ccfe0a
bluegrassrules <20289132+bluegrassrules@users.noreply.github.com>
2019-09-03 12:38:16 -0400

    Trying Part 1 again with ls-store.js.

--- js/main.js
+++ js/main.js
@@ -1,58 +1,33 @@
 var app = {
 
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
+		showAlert: function (message, title) {
+		    if (navigator.notification) {
+		        navigator.notification.alert(message, null, title, 'OK');
+		    } else {
+		        alert(title ? (title + ": " + message) : message);
+		    }
+		},
 
-    registerEvents: function() {
-        $(window).on('hashchange', $.proxy(this.route, this));
-        $('body').on('mousedown', 'a', function(event) {
-            $(event.target).addClass('tappable-active');
+    findByName: function() {
+        console.log('findByName');
+        this.store.findByName($('.search-key').val(), function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
         });
-        $('body').on('mouseup', 'a', function(event) {
-            $(event.target).removeClass('tappable-active');
-        });
     },
 
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(
-                message,
-                null, // callback
-                title,
-                'OK' // Button label
-            );
-        } else {
-            alert(title + ": " + message);
-        }
-    },
-
-    route: function() {
-        var hash = window.location.hash;
-        if (!hash) {
-            $('body').html(new HomeView(this.store).render().el);
-            return;
-        }
-        var match = hash.match(app.detailsURL);
-        if (match) {
-            this.store.findById(Number(match[1]), function(employee) {
-                $('body').html(new EmployeeView(employee).render().el);
-            });
-        }
-    },
-
-    initialize: function() {
-        var self = this;
-        this.detailsURL = /^#employees\/(\d{1,})/;
-        this.registerEvents();
-        this.store = new MemoryStore(function() {
-            self.route();
-        });
-    }
+initialize: function() {
+    var self = this;
+    this.store = new LocalStorageStore(function() {
+        self.showAlert('Store Initialized', 'Info');
+    });
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+}
 
 };
 
